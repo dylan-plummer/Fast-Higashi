e4dabb6950d2dfb5d9ffc94b2c6d93bc2e5fc44b
Dylan Plummer <dylanmplummer@gmail.com>
2023-01-11 20:33:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Fast-Higashi: Ultrafast and interpretable single-cell 3D genome analysis',
     url='https://github.com/ma-compbio/Fast-Higashi',
     include_package_data=True,
-    python_requires='>=3.9',
+    python_requires='>=3.7',
     packages=find_packages(),
     install_requires=[],
     extras_require={},
